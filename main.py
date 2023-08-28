01cf7d54f1945c40b4dfabef82f086dcb962a6f5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-27 17:36:51 -0700

    Create loop for adding dealt cards to user and computer lists

--- main.py
+++ main.py
@@ -39,14 +39,19 @@ import random
 
 
 def deal_card():
+    """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-# user_cards = []
-# computer_cards = []
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 # Hint 6: Create a function called calculate_score() that takes a List of cards as input
 # and returns the score.
