e4bddc91f1db565c953d85d179f60274bf0ea2e7
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-08-28 20:19:05 -0700

    Print user and computer's final hands

--- main.py
+++ main.py
@@ -115,6 +115,8 @@ while computer_score != 0 and computer_score < 17:
     computer_cards.append(deal_card())
     computer_score = calculate_score(computer_cards)
 
+print(f"Your final hand: {user_cards}, final score: {user_score}")
+print(f"Computer's final hand: {computer_score}, final score: {computer_score}")
 print(compare(user_score, computer_score))
 
 # Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
